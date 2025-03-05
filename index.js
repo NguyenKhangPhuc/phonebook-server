8954ad1f0dcbf2829868e792ad8188bc627107d4
Khang Phuc Nguyen <nkpnkpnkp2005@gmail.com>
2025-03-05 16:37:55 +0200

    Moving dist file from front-end to back-end for deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('post-data', (req) => req.method == 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :post-data'))
